7866602000b94b512e71cc350baafe963a15a327
meta100py (Valen's 2nd account) <meta100py@gmail.com>
2025-05-11 19:43:19 -0300

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -15,6 +15,9 @@ function generatePoem(event) {
   let context =
     "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Do not include 4 quotation marks at the start and in the end. Do not include the word html in the beginning. Sigh the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
   axios.get(apiUrl).then(displayPoem);
 }
 let poemFormElement = document.querySelector("#poem-generator-form");
