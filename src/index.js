792026669b8bfb34bee5424bf4778c751b6b2f2a
meta100py (Valen's 2nd account) <meta100py@gmail.com>
2025-05-11 16:18:29 -0300

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "bdbe141f0f305d4eao50fcf7t80ff6bb";
+  let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Do not include 4 quotation marks at the start and in the end. Do not include the word html in the beginning. Sigh the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayPoem);
 }
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
